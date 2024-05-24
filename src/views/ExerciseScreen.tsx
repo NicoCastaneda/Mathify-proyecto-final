a808f2243bce53711348e01ea786bc03719ba60c
Nicolás Castañeda <nicolascasol@unisabana.edu.co>
2024-05-24 17:45:17 -0500

    Actualización de estilos en ExcersiceScreen

--- src/views/ExerciseScreen.tsx
+++ src/views/ExerciseScreen.tsx
@@ -35,10 +35,32 @@ export default function ExerciseScreen() {
 
     return (
         <View style={styles.container}>
-            <Text>ExerciseScreen</Text>
-            <Text>Equation</Text>
-            <Text>{equation}</Text>
-            <Text>Clues: {perfil.clues}</Text>
+            <LinearGradient
+                colors={['#ffffff', '#4B9EFF']}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 0, y: 1 }}
+                style={{ position: 'absolute', left: 0, right: 0, top: 0, height: '20%', borderRadius: 20 }}
+            />
+            <Text style={styles.title}>NOMBRE DE LA LECCION</Text>
+            <View style={styles.clues}>
+                <Text style={styles.getClueText2}>{perfil.clues}</Text>
+            <MaterialCommunityIcons name="lightbulb-on" size={30} color='black' />
+            </View>
+            
+
+            <View style={styles.excersiceContainer}>
+                <LinearGradient
+                    colors={["#C674F1", "#F22E7A"]}
+                    start={{ x: 0, y: 0 }}
+                    end={{ x: 1, y: 0 }}
+                    style={{ position: 'absolute', left: 0, right: 0, top: 0, height: '100%', borderRadius: 10 }}
+                />
+                <Text>*COMPONENTE DEL EJERCICIO*</Text>
+                <Text>Exercise</Text>
+                <Text>{equation}</Text>
+            </View>
+
+            <Text style={styles.stuckedText}>Stuck in an excercise?</Text>
             <View style={{ flex: 1 }}>
                 <TouchableOpacity
                     style={styles.getClue}
@@ -59,17 +81,17 @@ export default function ExerciseScreen() {
             </View>
 
             <Modal
-        animationType= "fade"
-        visible={modalVisible}
-        onRequestClose={() => {
-          setModalVisible(!modalVisible);
-        }}
-      >
-        <Text>oops, you don't have any clues left</Text>
-        <TouchableOpacity onPress={() => setModalVisible(false)}>
-          <Text>Close</Text>
-        </TouchableOpacity>
-      </Modal>
+                animationType="fade"
+                visible={modalVisible}
+                onRequestClose={() => {
+                    setModalVisible(!modalVisible);
+                }}
+            >
+                <Text>oops, you don't have any clues left</Text>
+                <TouchableOpacity onPress={() => setModalVisible(false)}>
+                    <Text>Close</Text>
+                </TouchableOpacity>
+            </Modal>
         </View>
     );
 }
@@ -105,5 +127,46 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         fontSize: 18,
         textAlignVertical: 'center',
+    },
+    getClueText2: {
+        color: 'black',
+        fontWeight: 'bold',
+        fontSize: 18,
+        textAlignVertical: 'center',
+    },
+    stuckedText: {
+        position: 'absolute',
+        bottom: '9%',
+        left: '15%',
+        right: '15%',
+        textAlign: 'left',
+        fontSize: 15,
+        fontWeight: 'light',
+    },
+    excersiceContainer: {
+        
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
+        height: 450,
+        width: 350,
+        backgroundColor: '#F5FCFF',
+        borderRadius: 10,
+        marginTop: 50,
+    },
+    title: {
+        marginLeft: 20,
+        marginTop: 20,
+        textAlign: 'left',
+        fontSize: 25,
+        fontWeight: 'bold',
+    },
+    clues: {
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 8,
+        alignSelf: 'flex-end',
+        marginRight: 20,
+        marginTop: 30,
     }
 });
