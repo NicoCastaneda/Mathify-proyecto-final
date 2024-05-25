af6629ea5d39fbef711b5a50916bc38055618ae4
CharlieBello <carlosbeor@unisabana.edu.co>
2024-05-25 13:31:38 -0500

    Primer avance Image Picker
    --Dependencias añadidas--
    expo-image-picker
    
    --Archivos modificados--
    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore} from 'firebase/firestore/lite'
+import {getStorage, ref, uploadBytesResumable, getDownloadURL, listAll} from 'firebase/storage'
 
 
 export const firebaseConfig = {
@@ -12,6 +13,51 @@ export const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const dbInstance = getFirestore(app);
+const dbInstance = getFirestore(app);
+const imageStorage = getStorage(app);
 
+const listFiles = async () => {
+  const storage = getStorage(app);
+
+  // Create a reference under which you want to list
+  const listRef = ref(storage, "images");
+
+  // Find all the prefixes and items.
+  const listResp = await listAll(listRef);
+  return listResp.items;
+};
+
+const uploadToFirebase = async (uri, name, onProgress) => {
+  const fetchResponse = await fetch(uri);
+  const theBlob = await fetchResponse.blob();
+
+  const imageRef = ref(getStorage(app), `images/${name}`);
+
+  const uploadTask = uploadBytesResumable(imageRef, theBlob);
+
+  return new Promise((resolve, reject) => {
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        onProgress && onProgress(progress);
+      },
+      (error) => {
+        // Handle unsuccessful uploads
+        console.log(error);
+        reject(error);
+      },
+      async () => {
+        const downloadUrl = await getDownloadURL(uploadTask.snapshot.ref);
+        resolve({
+          downloadUrl,
+          metadata: uploadTask.snapshot.metadata,
+        });
+      }
+    );
+  });
+};
+
+export {dbInstance, imageStorage, uploadToFirebase, listFiles}
 export default app;
